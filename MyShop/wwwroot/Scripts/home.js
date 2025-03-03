8e23f8fb70d490fa753f920b533f2b29ae2a5b13
טובה שטיינר <TOVA143296@GMAIL.COM>
2025-03-04 01:27:08 +0200

    04/03/2025

--- MyShop/wwwroot/Scripts/home.js
+++ MyShop/wwwroot/Scripts/home.js
@@ -1,4 +1,12 @@
-﻿const hideSignInWhenUserIsIn = () => {
+﻿const onNewUserClick = () => {
+    const loginForm = document.querySelector('.loginForm'); // טופס הכניסה
+    const signInForm = document.querySelector('.signIn'); // טופס הרישום
+
+    // מסתיר את טופס הכניסה ומציג את טופס הרישום
+    loginForm.style.display = "none";
+    signInForm.style.display = "block";
+}
+const hideSignInWhenUserIsIn = () => {
     const hide = document.querySelector(".signIn");
     hide.classList.remove("signIn");
 };
@@ -6,6 +14,20 @@
 const hideUpdate = () => {
     const hide = document.querySelector(".update");
     hide.classList.remove("update");
+};
+const showUpdate = () => {
+    // בודק אם יש id של משתמש בסשן
+    const userId = sessionStorage.getItem("userId");
+
+    if (!userId) {
+        alert("You must log in first")
+
+        window.location.href = "home.html"; // הכנס את שם הדף של טופס הכניסה
+    } else {
+        // אם יש id, הצג את טופס העדכון
+        const updateForm = document.querySelector(".update");
+        updateForm.style.display = "block"; // מציג את טופס העדכון
+    }
 };
 
 const getAllUserDetails = () => {
@@ -32,7 +54,7 @@ const validationCheck = async (user) => {
     if (user.lastName.length < 2 || user.lastName.length > 20) 
         return "Last name must be between 2 to 20 characters";
     
-    if (user.password.length > 20)
+    if (user.password.length > 10)
         return "Password must be shorter than 20 characters";
     
     const level = await checkPassword();
@@ -107,13 +129,14 @@ const logIn = async () => {
 };
 
 const updateDetails = async () => {
+    debugger
     const newUser = getAllUserDetails();
     const res = await validationCheck(newUser);
     if (res !== "ok") {
         return alert(res);
     }
     try {
-        const responsePut = await fetch(`api/Users/${sessionStorage.getItem("id")}`, {
+        const responsePut = await fetch(`api/Users/${JSON.parse( sessionStorage.getItem("id"))}`, {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(newUser)
@@ -123,6 +146,6 @@ const updateDetails = async () => {
         }
         alert("User updated successfully");
     } catch (error) {
-        alert(`HTTP error! status ${responsePut.status}`);
+        alert(`HTTP error! status ${error.message}`);
     }
 };
